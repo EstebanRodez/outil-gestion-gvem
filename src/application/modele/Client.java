ac649d64e9fe03205c48ae4e06c348a3672cdf2d
Esteban Vroemen <esteban.vroemen@iut-rodez.fr>
2024-10-08 13:31:37 +0200

    feat: Client

---
+++ src/application/modele/Client.java
@@ -0,0 +1,19 @@
+/*
+ * Client.java                           
+ * 8 oct. 2024
+ * IUT de Rodez, pas de copyright
+ */
+package application.modele;
+
+/**
+ * Un client est représenté avec un intitulé et un numéro de 
+ * téléphone. Un client a forcément lié à une visite qu'il a réservé.
+ * L’intitulé est un texte libre qui pourrait être un nom et un 
+ * prénom et/ou le nom d’un organisme (par exemple 
+ * « M.Jacques Dupont » ou « Les amis des musées de Rodez » ou encore
+ * « M. Jacques Dupont pour les amis des musées de Rodez »)
+ * @version 1.0
+ */
+public class Client {
+
+}
