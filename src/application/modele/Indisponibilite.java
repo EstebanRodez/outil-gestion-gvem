4d9a4a9fa9e9d3a3d747c413932e2246d835cb9b
Esteban Vroemen <esteban.vroemen@iut-rodez.fr>
2024-10-11 12:00:52 +0200

    feat: Indisponibilite, tests et docs

---
+++ src/application/modele/Indisponibilite.java
@@ -0,0 +1,134 @@
+/*
+ * Indisponibilite.java                           
+ * 10 oct. 2024
+ * IUT de Rodez, pas de copyright
+ */
+package application.modele;
+
+import java.time.LocalDate;
+
+/**
+ * Représente une période d'indisponibilité avec une date de début
+ * obligatoire et une date de fin optionnelle.<br>
+ * Elle gère les périodes d'indisponibilité en définissant une 
+ * plage temporelle pendant laquelle une personne est indisponible. 
+ * Si la date de fin n'est pas renseignée, la période
+ * d'indisponibilité sera basée uniquement sur la date du début
+ * (= 1 jour d'indisponibilité).
+ * @author Esteban Vroemen
+ * @version 1.0
+ */
+public class Indisponibilite {
+    
+    private static final String ERREUR_DATE_DEBUT_INVALIDE =
+    """
+    Impossible de créer une indisponibilité.
+    La référence de la date de début d'une  indisponibilité
+    ne doit pas être nulle.                 
+    """;
+    
+    private static final String ERREUR_DATE_FIN_INVALIDE =
+    """
+    Impossible de créer une indisponibilité.
+    La référence de la date de fin d'une indisponibilité ne doit pas être nulle.                
+    """;
+    
+    private static final String ERREUR_DATE_FIN_INFERIEUR =
+    """
+    Impossible de créer une indisponibilité.
+    La date de fin d'une indisponibilité ne doit pas être inférieure ou égale
+    à la date de début.                
+    """;
+    
+    private LocalDate dateDebut;
+    
+    private LocalDate dateFin;
+    
+    /**
+     * Initialise une indisponibilité avec une date de début.
+     * La date de fin est ignoré. La période d'indisponilité sera
+     * sur un jour uniquement.
+     * 
+     * @param dateDebut La date à laquelle commence l'indisponibilité
+     *                  ou le jour de l'indisponibilité
+     * @throws IllegalArgumentException si la référence de la date de début est nulle
+     */
+    public Indisponibilite(LocalDate dateDebut) {
+        
+        if (dateDebut == null) {
+            throw new IllegalArgumentException(ERREUR_DATE_DEBUT_INVALIDE);
+        }
+        
+        this.dateDebut = dateDebut;
+    }
+    
+    /**
+     * Initialise une indisponibilité avec une date de début et une
+     * date de fin.
+     * 
+     * @param dateDebut La date à laquelle commence l'indisponibilité
+     * @param dateFin La date à laquelle se termine l'indisponibilité
+     * @throws IllegalArgumentException si la référence de la date de
+     *                                  début est nulle
+     * @throws IllegalArgumentException si la référence de la date de
+     *                                  fin est nulle
+     * @throws IllegalArgumentException si la date de fin est
+     *                                  inférieur ou égale à la date
+     *                                  de début
+     */
+    public Indisponibilite(LocalDate dateDebut, LocalDate dateFin) {
+        
+        if (dateDebut == null) {
+            throw new IllegalArgumentException(ERREUR_DATE_DEBUT_INVALIDE);
+        }
+        
+        if (dateFin == null) {
+            throw new IllegalArgumentException(ERREUR_DATE_FIN_INVALIDE);
+        }
+        
+        if (dateFin.compareTo(dateDebut) <= 0) {
+            throw new IllegalArgumentException(ERREUR_DATE_FIN_INFERIEUR);
+        }
+        
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+    }
+
+    /**
+     * Retourne la date de fin de l'indisponibilité.
+     * @return dateFin La date de fin de l'indisponibilité, ou null
+     *                 si aucune date de fin n'a été définie.
+     */
+    public LocalDate getDateFin() {
+        return dateFin;
+    }
+
+    /**
+     * Définit la date de fin de l'indisponibilité.
+     * @param dateFin La date à laquelle se termine l'indisponibilité
+     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException
+     */
+    public void setDateFin(LocalDate dateFin) {
+        
+        if (dateFin == null) {
+            throw new IllegalArgumentException(ERREUR_DATE_FIN_INVALIDE);
+        }
+        
+        if (dateFin.compareTo(dateDebut) <= 0) {
+            throw new IllegalArgumentException(ERREUR_DATE_FIN_INFERIEUR);
+        }
+        
+        this.dateFin = dateFin;
+    }
+
+    /**
+     * Retourne la date de début de l'indisponibilité.
+     * @return dateDebut La date de début de l'indisponibilité
+     */
+    public LocalDate getDateDebut() {
+        return dateDebut;
+    }
+    
+    
+}
---
+++ src/test/modele/TestIndisponibilite.java
@@ -0,0 +1,240 @@
+/*
+ * TestIndisponibilite.java                           
+ * 10 oct. 2024
+ * IUT de Rodez, pas de copyright
+ */
+package test.modele;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import application.modele.Indisponibilite;
+
+/**
+ * Classe de test pour {@link application.modele.Indisponibilite}
+ * @author Esteban Vroemen
+ */
+class TestIndisponibilite {
+    
+    private final LocalDate DATE_VALIDE = LocalDate.of(2024, 10, 9);
+    
+    private final Indisponibilite[] INDISPONIBILITES_VALIDES =
+    {
+        new Indisponibilite(LocalDate.of(2005, 5, 28)),
+        new Indisponibilite(LocalDate.of(2005, 3, 21)),
+        new Indisponibilite(LocalDate.of(2005, 8, 14)),
+        new Indisponibilite(LocalDate.of(2005, 11, 20)),  
+        new Indisponibilite(LocalDate.of(2005, 5, 28),
+                            LocalDate.of(2005, 6, 28)),
+        new Indisponibilite(LocalDate.of(2005, 3, 21),
+                            LocalDate.of(2005, 4, 21)),
+        new Indisponibilite(LocalDate.of(2005, 8, 14),
+                            LocalDate.of(2005, 9, 14)),
+        new Indisponibilite(LocalDate.of(2005, 11, 20),
+                            LocalDate.of(2005, 12, 20)), 
+    };
+
+    /**
+     * Méthode de test pour 
+     * {@link application.modele.Indisponibilite#Indisponibilite(java.time.LocalDate)}
+     * et
+     * {@link application.modele.Indisponibilite#Indisponibilite(java.time.LocalDate, java.time.LocalDate)}
+     * Cas uniquement invalides
+     */
+    @Test
+    void testIndisponibiliteInvalide() {
+        
+        /*
+         * Tests spécifiques au constructeur à 1 argument
+         */
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Indisponibilite(null));
+        
+        /*
+         * Tests spécifiques au constructeur à 2 argument
+         */
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Indisponibilite(DATE_VALIDE, null));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Indisponibilite(null, DATE_VALIDE));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Indisponibilite(DATE_VALIDE, DATE_VALIDE));
+        assertThrows(IllegalArgumentException.class,
+                     () -> new Indisponibilite(DATE_VALIDE,
+                                               LocalDate.of(2005, 5, 28)));
+        assertThrows(IllegalArgumentException.class,
+                     () -> new Indisponibilite(DATE_VALIDE,
+                                               LocalDate.of(2005, 3, 21)));
+        assertThrows(IllegalArgumentException.class,
+                     () -> new Indisponibilite(DATE_VALIDE,
+                                               LocalDate.of(2005, 8, 14)));
+        assertThrows(IllegalArgumentException.class,
+                     () -> new Indisponibilite(DATE_VALIDE,
+                                               LocalDate.of(2005, 11, 20)));
+    }
+
+    /**
+     * Méthode de test pour 
+     * {@link application.modele.Indisponibilite#Indisponibilite(java.time.LocalDate)}
+     * et
+     * {@link application.modele.Indisponibilite#Indisponibilite(java.time.LocalDate, java.time.LocalDate)}
+     * Cas uniquement valides
+     */
+    @Test
+    void testIndisponibiliteValide() {
+        
+        /*
+         * Tests spécifiques au constructeur à 1 argument
+         */
+        assertDoesNotThrow(() -> new Indisponibilite(DATE_VALIDE));
+        assertDoesNotThrow(() -> new Indisponibilite(LocalDate.of(2005, 5, 28)));
+        assertDoesNotThrow(() -> new Indisponibilite(LocalDate.of(2005, 3, 21)));
+        assertDoesNotThrow(() -> new Indisponibilite(LocalDate.of(2005, 8, 14)));
+        assertDoesNotThrow(() -> new Indisponibilite(LocalDate.of(2005, 11, 20)));
+        
+        /*
+         * Tests spécifiques au constructeur à 2 argument
+         */
+        assertDoesNotThrow(() -> new Indisponibilite(LocalDate.of(2005, 5, 28),
+                                                     DATE_VALIDE));
+        assertDoesNotThrow(() -> new Indisponibilite(LocalDate.of(2005, 3, 21),
+                                                     DATE_VALIDE));
+        assertDoesNotThrow(() -> new Indisponibilite(LocalDate.of(2005, 8, 14),
+                                                     DATE_VALIDE));
+        assertDoesNotThrow(() -> new Indisponibilite(LocalDate.of(2005, 11, 20),
+                                                     DATE_VALIDE));
+    }
+
+    /**
+     * Méthode de test pour
+     * {@link application.modele.Indisponibilite#getDateFin()}.
+     */
+    @Test
+    void testGetDateFin() {
+        
+        assertEquals(null, INDISPONIBILITES_VALIDES[0].getDateFin());
+        assertEquals(null, INDISPONIBILITES_VALIDES[1].getDateFin());
+        assertEquals(null, INDISPONIBILITES_VALIDES[2].getDateFin());
+        assertEquals(null, INDISPONIBILITES_VALIDES[3].getDateFin());
+        assertEquals(LocalDate.of(2005, 6, 28),
+                     INDISPONIBILITES_VALIDES[4].getDateFin());
+        assertEquals(LocalDate.of(2005, 4, 21),
+                     INDISPONIBILITES_VALIDES[5].getDateFin());
+        assertEquals(LocalDate.of(2005, 9, 14),
+                     INDISPONIBILITES_VALIDES[6].getDateFin());
+        assertEquals(LocalDate.of(2005, 12, 20),
+                     INDISPONIBILITES_VALIDES[7].getDateFin());
+        
+        assertNotEquals(LocalDate.of(2005, 6, 28),
+                        INDISPONIBILITES_VALIDES[0].getDateFin());
+        assertNotEquals(LocalDate.of(2005, 4, 28),
+                        INDISPONIBILITES_VALIDES[1].getDateFin());
+        assertNotEquals(LocalDate.of(2005, 11, 28),
+                        INDISPONIBILITES_VALIDES[2].getDateFin());
+        assertNotEquals(LocalDate.of(2005, 9, 28),
+                        INDISPONIBILITES_VALIDES[3].getDateFin());
+        assertNotEquals(null, INDISPONIBILITES_VALIDES[4].getDateFin());
+        assertNotEquals(null, INDISPONIBILITES_VALIDES[5].getDateFin());
+        assertNotEquals(null, INDISPONIBILITES_VALIDES[6].getDateFin());
+        assertNotEquals(null, INDISPONIBILITES_VALIDES[7].getDateFin());
+    }
+
+    /**
+     * Méthode de test pour
+     * {@link application.modele.Indisponibilite#setDateFin(java.time.LocalDate)}.
+     */
+    @Test
+    void testSetDateFin() {
+        
+        assertThrows(IllegalArgumentException.class, 
+                    () -> INDISPONIBILITES_VALIDES[0].setDateFin(null));
+        assertThrows(IllegalArgumentException.class, 
+                    () -> INDISPONIBILITES_VALIDES[0].setDateFin(
+                            LocalDate.of(2005, 4, 28)));
+        assertThrows(IllegalArgumentException.class, 
+                    () -> INDISPONIBILITES_VALIDES[1].setDateFin(
+                            LocalDate.of(2005, 2, 21)));
+        assertThrows(IllegalArgumentException.class, 
+                    () -> INDISPONIBILITES_VALIDES[2].setDateFin(
+                            LocalDate.of(2005, 7, 14)));
+        assertThrows(IllegalArgumentException.class, 
+                    () -> INDISPONIBILITES_VALIDES[3].setDateFin(
+                            LocalDate.of(2005, 10, 20)));
+        assertThrows(IllegalArgumentException.class, 
+                    () -> INDISPONIBILITES_VALIDES[0].setDateFin(
+                            LocalDate.of(2005, 5, 28)));
+        assertThrows(IllegalArgumentException.class, 
+                    () -> INDISPONIBILITES_VALIDES[1].setDateFin(
+                            LocalDate.of(2005, 3, 21)));
+        assertThrows(IllegalArgumentException.class, 
+                    () -> INDISPONIBILITES_VALIDES[2].setDateFin(
+                            LocalDate.of(2005, 8, 14)));
+        assertThrows(IllegalArgumentException.class, 
+                    () -> INDISPONIBILITES_VALIDES[3].setDateFin(
+                            LocalDate.of(2005, 11, 20)));
+        
+        assertDoesNotThrow(() -> INDISPONIBILITES_VALIDES[0].setDateFin(
+                LocalDate.of(2005, 6, 28)));
+        assertDoesNotThrow(() -> INDISPONIBILITES_VALIDES[1].setDateFin(
+                LocalDate.of(2005, 4, 21)));
+        assertDoesNotThrow(() -> INDISPONIBILITES_VALIDES[2].setDateFin(
+                LocalDate.of(2005, 9, 14)));
+        assertDoesNotThrow(() -> INDISPONIBILITES_VALIDES[3].setDateFin(
+                LocalDate.of(2005, 12, 20)));
+    }
+
+    /**
+     * Méthode de test pour
+     * {@link application.modele.Indisponibilite#getDateDebut()}.
+     */
+    @Test
+    void testGetDateDebut() {
+        
+        assertEquals(LocalDate.of(2005, 5, 28),
+                     INDISPONIBILITES_VALIDES[0].getDateDebut());
+        assertEquals(LocalDate.of(2005, 3, 21),
+                     INDISPONIBILITES_VALIDES[1].getDateDebut());
+        assertEquals(LocalDate.of(2005, 8, 14),
+                     INDISPONIBILITES_VALIDES[2].getDateDebut());
+        assertEquals(LocalDate.of(2005, 11, 20),
+                     INDISPONIBILITES_VALIDES[3].getDateDebut());
+        assertEquals(LocalDate.of(2005, 5, 28),
+                     INDISPONIBILITES_VALIDES[4].getDateDebut());
+        assertEquals(LocalDate.of(2005, 3, 21),
+                     INDISPONIBILITES_VALIDES[5].getDateDebut());
+        assertEquals(LocalDate.of(2005, 8, 14),
+                     INDISPONIBILITES_VALIDES[6].getDateDebut());
+        assertEquals(LocalDate.of(2005, 11, 20),
+                     INDISPONIBILITES_VALIDES[7].getDateDebut());
+        
+        assertNotEquals(null, INDISPONIBILITES_VALIDES[0].getDateDebut());
+        assertNotEquals(null, INDISPONIBILITES_VALIDES[1].getDateDebut());
+        assertNotEquals(null, INDISPONIBILITES_VALIDES[2].getDateDebut());
+        assertNotEquals(null, INDISPONIBILITES_VALIDES[3].getDateDebut());
+        assertNotEquals(null, INDISPONIBILITES_VALIDES[4].getDateDebut());
+        assertNotEquals(null, INDISPONIBILITES_VALIDES[5].getDateDebut());
+        assertNotEquals(null, INDISPONIBILITES_VALIDES[6].getDateDebut());
+        assertNotEquals(null, INDISPONIBILITES_VALIDES[7].getDateDebut());
+        assertNotEquals(LocalDate.of(2005, 6, 28),
+                        INDISPONIBILITES_VALIDES[0].getDateDebut());
+        assertNotEquals(LocalDate.of(2005, 4, 28),
+                        INDISPONIBILITES_VALIDES[1].getDateDebut());
+        assertNotEquals(LocalDate.of(2005, 11, 28),
+                        INDISPONIBILITES_VALIDES[2].getDateDebut());
+        assertNotEquals(LocalDate.of(2005, 9, 28),
+                        INDISPONIBILITES_VALIDES[3].getDateDebut());
+        assertNotEquals(LocalDate.of(2005, 6, 28),
+                        INDISPONIBILITES_VALIDES[4].getDateDebut());
+        assertNotEquals(LocalDate.of(2005, 4, 28),
+                        INDISPONIBILITES_VALIDES[5].getDateDebut());
+        assertNotEquals(LocalDate.of(2005, 11, 28),
+                        INDISPONIBILITES_VALIDES[6].getDateDebut());
+        assertNotEquals(LocalDate.of(2005, 9, 28),
+                        INDISPONIBILITES_VALIDES[7].getDateDebut());
+
+    }
+
+}
