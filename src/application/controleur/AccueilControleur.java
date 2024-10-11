5f94b4fc8431c6d489f46ca92ea5b65a1a39e5aa
BaptisteThnrs <baptiste@thenieres.fr>
2024-10-11 21:00:50 +0200

    refactor: renommage de toute les classes/fichiers contenant le mot 'acceuil' en 'accueil

---
+++ src/application/controleur/AccueilControleur.java
@@ -0,0 +1,56 @@
+/*
+ * AcceuilControleur.java                           
+ * 11 oct. 2024
+ * IUT de Rodez, pas de copyright
+ */
+package application.controleur;
+
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+
+/**
+ * TODO commenter la responsabilité de cette class (SRP)
+ */
+public class AccueilControleur {
+    
+    @FXML
+    private Button btnAide;
+
+    @FXML
+    private Button btnConsulterDonnees;
+
+    @FXML
+    private Button btnExporter;
+
+    @FXML
+    private Button btnImporter;
+
+    @FXML
+    private Button btnQuitter;
+
+    @FXML
+    void btnAideAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnConsulterDonneesAction(ActionEvent event) {
+        //TODO renvoyait vers la page consulterDonnesVue.fxml
+    }
+
+    @FXML
+    void btnExporterAction(ActionEvent event) {
+        //TODO renvoyait vers la page exporter.fxml
+    }
+
+    @FXML
+    void btnImporterAction(ActionEvent event) {
+        //TODO renvoyait vers la page importerVue.fxml
+    }
+
+    @FXML
+    void btnQuitterAction(ActionEvent event) {
+        //TODO quitter l'application
+    }
+
+}
