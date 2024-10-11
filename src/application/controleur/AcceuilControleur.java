2e7bdf6ee147492d274e2439dedb616d79b792af
BAPTISTE THENIERES <baptiste.thenieres@iut.local>
2024-10-11 14:40:04 +0200

    ajout du package 'controleur' avec la classe AcceuilControleur

---
+++ src/application/controleur/AcceuilControleur.java
@@ -0,0 +1,53 @@
+/*
+ * AcceuilControleur.java                           
+ * 11 oct. 2024
+ * IUT de Rodez, pas de copyright
+ */
+package application.controleur;
+
+/**
+ * TODO commenter la responsabilité de cette class (SRP)
+ */
+public class AcceuilControleur {
+    
+    @FXML
+    private Button btnAide;
+
+    @FXML
+    private Button btnConsulterDonnees;
+
+    @FXML
+    private Button btnExporter;
+
+    @FXML
+    private Button btnImporter;
+
+    @FXML
+    private Button btnQuitter;
+
+    @FXML
+    void btnAideAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnConsulterDonneesAction(ActionEvent event) {
+        //TODO renvoyait vers la page consulterDonnesVue.fxml
+    }
+
+    @FXML
+    void btnExporterAction(ActionEvent event) {
+        //TODO renvoyait vers la page exporter.fxml
+    }
+
+    @FXML
+    void btnImporterAction(ActionEvent event) {
+        //TODO renvoyait vers la page importerVue.fxml
+    }
+
+    @FXML
+    void btnQuitterAction(ActionEvent event) {
+        //TODO quitter l'application
+    }
+
+}
