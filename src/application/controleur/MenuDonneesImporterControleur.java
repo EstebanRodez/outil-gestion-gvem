b502dd4bff1da11d4313c9904cbfe45c53e74a39
BaptisteThnrs <baptiste@thenieres.fr>
2024-10-13 02:25:50 +0200

    feat: vue et controleur du menu donnees importer

---
+++ src/application/controleur/MenuDonneesImporterControleur.java
@@ -0,0 +1,66 @@
+/*
+ * menuDonneesImporterControleur.java                           
+ * 13 oct. 2024
+ * IUT de Rodez, pas de copyright
+ */
+package application.controleur;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * TODO commenter la responsabilité de cette class (SRP)
+ */
+public class MenuDonneesImporterControleur {
+    
+    @FXML
+    private Button btnAccueil;
+
+    @FXML
+    private Button btnConferencier;
+
+    @FXML
+    private Button btnDonnesCalculees;
+
+    @FXML
+    private Button btnExposition;
+
+    @FXML
+    private Button btnStatistique;
+
+    @FXML
+    private Button btnVisite;
+
+    @FXML
+    void btnAccueilAction(ActionEvent event) {
+        //TODO renvois vers la page accueil
+    }
+
+    @FXML
+    void btnConferencierAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDonnesCalculeesAction(ActionEvent event) {
+        //TODO renvois vers la page menuDonnesCalculees
+    }
+
+    @FXML
+    void btnExpositionAction(ActionEvent event) {
+        
+    }
+
+    @FXML
+    void btnStatistiqueAction(ActionEvent event) {
+        //TODO renvois vers la page menuStatistique
+    }
+
+    @FXML
+    void btnVisiteAction(ActionEvent event) {
+
+    }
+
+
+}
