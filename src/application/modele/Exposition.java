dd749d9e0ad87a694d818cc1e88d26d8e545813f
Esteban Vroemen <esteban.vroemen@gmail.com>
2024-10-13 18:12:15 +0200

    style: indentation colonne 80 max et alignements

--- src/application/modele/Exposition.java
+++ src/application/modele/Exposition.java
@@ -25,115 +25,142 @@ package application.modele;
  * @version 1.0
  */
 public class Exposition {
-	private static String ERREUR_IDENTIFIANT_INVALIDE =
+    
+    private static String ERREUR_IDENTIFIANT_INVALIDE =
     """
     Impossible de créer une exposition.
     L'identifiant de l'exposition ne doit pas être nul ou vide.                
     """;
-	private static String ERREUR_INTITULE_INVALIDE =
+    
+    private static String ERREUR_INTITULE_INVALIDE =
     """
     Impossible de créer une exposition.
     L'intitulé de l'exposition ne doit pas être nul ou vide.                
     """;
-	private static String ERREUR_PERIODE_INVALIDE =
+    
+    private static String ERREUR_PERIODE_INVALIDE =
     """
     Impossible de créer une exposition.
     La période de l'exposition ne doit pas être nul ou vide.                
     """;
-	private static String ERREUR_NBOEUVRE_INVALIDE =
+    
+    private static String ERREUR_NBOEUVRE_INVALIDE =
     """
     Impossible de créer une exposition.
     Le nombre d'oeuvre dans l'exposition ne doit pas être nul ou vide.                
     """;
-	private static String ERREUR_MOTSCLES_INVALIDE =
+    
+    private static String ERREUR_MOTSCLES_INVALIDE =
     """
     Impossible de créer une exposition.
     La liste de mots clés de l'exposition ne doit pas être nul ou vide.                
     """;
-	private static String ERREUR_RESUME_INVALIDE =
+    
+    private static String ERREUR_RESUME_INVALIDE =
     """
     Impossible de créer une exposition.
     Le résumé de l'exposition ne doit pas être nul ou vide.                
     """;
-	
-	private String identifiant;
-	private String intitule;
-	private int periodeDeb;
-	private int periodeFin;
-	private int nbOeuvre;
-	private String[] motsCles;
-	private String resume;
-	
-	/**
-     * Initialise une nouvelle exposition avec un identifiant, un intitulé,
-     *  une période, un nombre d'oeuvres, une liste de mots clés et un résumé
+
+    private String identifiant;
+    
+    private String intitule;
+    
+    private int periodeDeb;
+    
+    private int periodeFin;
+    
+    private int nbOeuvre;
+    
+    private String[] motsCles;
+    
+    private String resume;
+
+    /**
+     * Initialise une nouvelle exposition avec un identifiant,
+     * un intitulé, une période, un nombre d'oeuvres, une liste de
+     * mots clés et un résumé
      * @param identifiant l'identifiant/code de l'exposition
      * @param intitule le nom ou la désignation de l'exposition
-     * @param periodeDeb est le début de la période durant laquelle aura lieu 
-     * 		  l'exposition
-     * @param periodeFin est la fin de la période durant laquelle aura lieu 
-     * 		  l'exposition
+     * @param periodeDeb est le début de la période durant laquelle
+     *                   aura lieu l'exposition
+     * @param periodeFin est la fin de la période durant laquelle aura
+     *                   lieu l'exposition
      * @param nbOeuvre le nombre d'oeuvre présent dans l'exposition
-     * @param motsCles liste de mots clés par lequels 
-     * 		  on peut retrouver l'exposition
+     * @param motsCles liste de mots clés par lequels on peut
+     *                 retrouver l'exposition
      * @param resume résumé de l'exposition
-     * @throws IllegalArgumentException si la référence de l'identifiant
-     *                                  est nul ou l'identifiant est vide
+     * @throws IllegalArgumentException si la référence de
+     *                                  l'identifiant est nul ou
+     *                                  l'identifiant est vide
      * @throws IllegalArgumentException si la référence de l'intitulé
-     *                                  est nul ou l'intitulé est vide
-     * @throws IllegalArgumentException si la référence de est nul ou vide
-     * @throws IllegalArgumentException si la référence du nombre d'oeuvre
-     *                                  est nul ou vide
-     * @throws IllegalArgumentException si la référence de la liste des 
-     * 									mots clés est nul ou vide
-     * @throws IllegalArgumentException si la référence du résumé
-     *                                  est nul ou vide
-     * @throws IllegalArgumentException si le nombre de mots clés est > 10
+     *                                  est nul ou l'intitulé est
+     *                                  vide
+     * @throws IllegalArgumentException si la référence de est nul ou
+     *                                  vide
+     * @throws IllegalArgumentException si la référence du nombre
+     *                                  d'oeuvre est nul ou vide
+     * @throws IllegalArgumentException si la référence de la liste
+     *                                  des mots clés est nul ou vide
+     * @throws IllegalArgumentException si la référence du résumé est
+     *                                  nul ou vide
+     * @throws IllegalArgumentException si le nombre de mots clés est
+     *                                  supérieur à 10
      */
-	public Exposition (String identifiant,String intitule, int periodeDeb, int periodeFin, int nbOeuvre, String[] motsCles, String resume) {
-		if(identifiant == null || identifiant.isBlank()) {
-			throw new IllegalArgumentException(ERREUR_IDENTIFIANT_INVALIDE);
-		}
-		if(intitule == null || identifiant.isBlank()) {
-			throw new IllegalArgumentException(ERREUR_INTITULE_INVALIDE);
-		}
-		if(periodeDeb <= 0 || periodeFin <= 0
-		   || periodeDeb > periodeFin) {
-			throw new IllegalArgumentException(ERREUR_PERIODE_INVALIDE);
-		}
-		if (nbOeuvre <= 0) {
-			throw new IllegalArgumentException(ERREUR_NBOEUVRE_INVALIDE);
-		}
-		if (motsCles == null || motsCles.length == 0 || motsCles.length > 10){
-			throw new IllegalArgumentException(ERREUR_MOTSCLES_INVALIDE);
-		} else {
-			for(int i = 0; i < motsCles.length ; i++) {
-				if(motsCles[i] == null || motsCles[i].isBlank()){
-					throw new IllegalArgumentException(ERREUR_MOTSCLES_INVALIDE);
-				}
-			}
-		}
-		if(resume == null || resume.isBlank()) {
-			throw new IllegalArgumentException(ERREUR_RESUME_INVALIDE);
-		}
-		
-		this.identifiant = identifiant;
-		this.intitule =intitule;
-		this.periodeDeb = periodeDeb;
-		this.periodeFin = periodeFin;
-		this.nbOeuvre = nbOeuvre;
-		this.motsCles = motsCles;
-		this.resume = resume;
-	}
-	
-	/**
+    public Exposition (String identifiant,String intitule, int periodeDeb,
+                       int periodeFin, int nbOeuvre, String[] motsCles,
+                       String resume) {
+        
+        if (identifiant == null || identifiant.isBlank()) {
+            throw new IllegalArgumentException(ERREUR_IDENTIFIANT_INVALIDE);
+        }
+        
+        if (intitule == null || identifiant.isBlank()) {
+            throw new IllegalArgumentException(ERREUR_INTITULE_INVALIDE);
+        }
+        
+        if (periodeDeb <= 0 || periodeFin <= 0 || periodeDeb > periodeFin) {
+            throw new IllegalArgumentException(ERREUR_PERIODE_INVALIDE);
+        }
+        
+        if (nbOeuvre <= 0) {
+            throw new IllegalArgumentException(ERREUR_NBOEUVRE_INVALIDE);
+        }
+        
+        if (motsCles == null || motsCles.length == 0 || motsCles.length > 10) {
+            
+            throw new IllegalArgumentException(ERREUR_MOTSCLES_INVALIDE);
+        } else {
+            
+            for (int i = 0; i < motsCles.length ; i++) {
+                if (motsCles[i] == null || motsCles[i].isBlank()){
+                    throw new IllegalArgumentException(
+                            ERREUR_MOTSCLES_INVALIDE);
+                }
+            }
+        }
+        
+        if (resume == null || resume.isBlank()) {
+            throw new IllegalArgumentException(ERREUR_RESUME_INVALIDE);
+        }
+
+        this.identifiant = identifiant;
+        this.intitule = intitule;
+        this.periodeDeb = periodeDeb;
+        this.periodeFin = periodeFin;
+        this.nbOeuvre = nbOeuvre;
+        this.motsCles = motsCles;
+        this.resume = resume;
+    }
+
+    /**
      * Récupère l'identifiant de l'exposition
      * @return l'identifiant de l'exposition
      */
     public String getIdentifiant() {
         return identifiant;
     }
-    
+
     /**
      * Récupère l'idntitulé de l'exposition
      * @return l'intitulé de l'exposition
@@ -141,7 +168,7 @@ public class Exposition {
     public String getIntitule() {
         return intitule;
     }
-	
+
     /**
      * Récupère la période du début de l'exposition
      * @return la période du début de l'exposition
@@ -149,7 +176,7 @@ public class Exposition {
     public int getPeriodeDeb() {
         return periodeDeb;
     }
-    
+
     /**
      * Récupère la période de fin de l'exposition
      * @return la période de fin de l'exposition
@@ -157,7 +184,7 @@ public class Exposition {
     public int getPeriodeFin() {
         return periodeFin;
     }
-    
+
     /**
      * Récupère le nombre d'oeuvre présentent dans l'exposition
      * @return le nombre d'oeuvre de l'exposition
@@ -165,7 +192,7 @@ public class Exposition {
     public int getNbOeuvre() {
         return nbOeuvre;
     }
-    
+
     /**
      * Récupère les mots clés définissant l'exposition
      * @return les mots clés de l'exposition
@@ -173,7 +200,7 @@ public class Exposition {
     public String[] getMotsCles() {
         return motsCles;
     }
-    
+
     /**
      * Récupère le résumé de l'exposition
      * @return le résumé de l'exposition
--- src/test/modele/TestExposition.java
+++ src/test/modele/TestExposition.java
@@ -15,15 +15,24 @@ import application.modele.Exposition;
  * Classe de Test de la classe Exposition
  */
 class TestExposition {
-    
+
+    private final Exposition[] EXPO_VALIDES = null;
+
     private final String IDENTIFIANT_EXPO_VALIDE = "E000001";
+    
     private final String INTITULE_EXPO_VALIDE = "Les paysages impressionnistes";
+    
     private final int PERIODEDEB_EXPO_VALIDE = 1880;
+    
     private final int PERIODEFIN_EXPO_VALIDE = 1895;
+    
     private final int NBOEUVRE_EXPO_VALIDE = 20;
-    private final String[] MOTSCLES_EXPO_VALIDE = 
-        {"paysage", "impressionnisme", "Monet", "Pissarro", "Sysley", "Signac"};
-    private final String RESUME_EXPO_VALIDE = "Très belle exposition sur l'impressionnisme.";
+    
+    private final String[] MOTSCLES_EXPO_VALIDE
+    = {"paysage", "impressionnisme", "Monet", "Pissarro", "Sysley", "Signac"};
+    
+    private final String RESUME_EXPO_VALIDE
+    = "Très belle exposition sur l'impressionnisme.";
 
     /**
      * Méthode de test pour 
@@ -33,99 +42,145 @@ class TestExposition {
      */
     @Test
     void testExpositionInvalide() {
+        
         // Test avec identifiant null ou vide
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(null, INTITULE_EXPO_VALIDE, PERIODEDEB_EXPO_VALIDE, 
-                                          PERIODEFIN_EXPO_VALIDE, NBOEUVRE_EXPO_VALIDE, 
-                                          MOTSCLES_EXPO_VALIDE, RESUME_EXPO_VALIDE));
+                     () -> new Exposition(null, INTITULE_EXPO_VALIDE, 
+                                          PERIODEDEB_EXPO_VALIDE, 
+                                          PERIODEFIN_EXPO_VALIDE,
+                                          NBOEUVRE_EXPO_VALIDE, 
+                                          MOTSCLES_EXPO_VALIDE,
+                                          RESUME_EXPO_VALIDE));
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition("", INTITULE_EXPO_VALIDE, PERIODEDEB_EXPO_VALIDE, 
-                                          PERIODEFIN_EXPO_VALIDE, NBOEUVRE_EXPO_VALIDE, 
-                                          MOTSCLES_EXPO_VALIDE, RESUME_EXPO_VALIDE));
-        
+                     () -> new Exposition("", INTITULE_EXPO_VALIDE, 
+                                          PERIODEDEB_EXPO_VALIDE, 
+                                          PERIODEFIN_EXPO_VALIDE,
+                                          NBOEUVRE_EXPO_VALIDE, 
+                                          MOTSCLES_EXPO_VALIDE,
+                                          RESUME_EXPO_VALIDE));
+
         // Test avec intitulé null ou vide
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, null, PERIODEDEB_EXPO_VALIDE, 
-                                          PERIODEFIN_EXPO_VALIDE, NBOEUVRE_EXPO_VALIDE, 
-                                          MOTSCLES_EXPO_VALIDE, RESUME_EXPO_VALIDE));
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, null, 
+                                          PERIODEDEB_EXPO_VALIDE, 
+                                          PERIODEFIN_EXPO_VALIDE,
+                                          NBOEUVRE_EXPO_VALIDE, 
+                                          MOTSCLES_EXPO_VALIDE,
+                                          RESUME_EXPO_VALIDE));
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, "", PERIODEDEB_EXPO_VALIDE, 
-                                          PERIODEFIN_EXPO_VALIDE, NBOEUVRE_EXPO_VALIDE, 
-                                          MOTSCLES_EXPO_VALIDE, RESUME_EXPO_VALIDE));
-        
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, "",
+                                          PERIODEDEB_EXPO_VALIDE, 
+                                          PERIODEFIN_EXPO_VALIDE,
+                                          NBOEUVRE_EXPO_VALIDE, 
+                                          MOTSCLES_EXPO_VALIDE,
+                                          RESUME_EXPO_VALIDE));
+
         // Test avec périodes invalides
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                          INTITULE_EXPO_VALIDE, 
                                           2025, 1880, NBOEUVRE_EXPO_VALIDE, 
-                                          MOTSCLES_EXPO_VALIDE, RESUME_EXPO_VALIDE));
+                                          MOTSCLES_EXPO_VALIDE,
+                                          RESUME_EXPO_VALIDE));
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                          -10, 1880, NBOEUVRE_EXPO_VALIDE, 
-                                          MOTSCLES_EXPO_VALIDE, RESUME_EXPO_VALIDE));
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                         INTITULE_EXPO_VALIDE, -10, 1880,
+                                         NBOEUVRE_EXPO_VALIDE,
+                                         MOTSCLES_EXPO_VALIDE,
+                                         RESUME_EXPO_VALIDE));
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                          1880, 0, NBOEUVRE_EXPO_VALIDE, 
-                                          MOTSCLES_EXPO_VALIDE, RESUME_EXPO_VALIDE));
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                          INTITULE_EXPO_VALIDE, 1880, 0, 
+                                          NBOEUVRE_EXPO_VALIDE, 
+                                          MOTSCLES_EXPO_VALIDE,
+                                          RESUME_EXPO_VALIDE));
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                          0, 0, NBOEUVRE_EXPO_VALIDE, 
-                                          MOTSCLES_EXPO_VALIDE, RESUME_EXPO_VALIDE));
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                          INTITULE_EXPO_VALIDE, 0, 0,
+                                          NBOEUVRE_EXPO_VALIDE, 
+                                          MOTSCLES_EXPO_VALIDE,
+                                          RESUME_EXPO_VALIDE));
 
         // Test avec nombre d'œuvres invalide
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                          PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE, 
-                                          0, MOTSCLES_EXPO_VALIDE, RESUME_EXPO_VALIDE));
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, 
+                                          INTITULE_EXPO_VALIDE, 
+                                          PERIODEDEB_EXPO_VALIDE,
+                                          PERIODEFIN_EXPO_VALIDE, 0,
+                                          MOTSCLES_EXPO_VALIDE,
+                                          RESUME_EXPO_VALIDE));
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                          PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE, 
-                                          -5, MOTSCLES_EXPO_VALIDE, RESUME_EXPO_VALIDE));
-        
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                          INTITULE_EXPO_VALIDE, 
+                                          PERIODEDEB_EXPO_VALIDE,
+                                          PERIODEFIN_EXPO_VALIDE, -5, 
+                                          MOTSCLES_EXPO_VALIDE, 
+                                          RESUME_EXPO_VALIDE));
+
         // Test avec liste de mots-clés invalide
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                          PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE, 
-                                          NBOEUVRE_EXPO_VALIDE, null, RESUME_EXPO_VALIDE));
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                          INTITULE_EXPO_VALIDE, 
+                                          PERIODEDEB_EXPO_VALIDE,
+                                          PERIODEFIN_EXPO_VALIDE, 
+                                          NBOEUVRE_EXPO_VALIDE, null,
+                                          RESUME_EXPO_VALIDE));
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                          PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE, 
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                          INTITULE_EXPO_VALIDE, 
+                                          PERIODEDEB_EXPO_VALIDE,
+                                          PERIODEFIN_EXPO_VALIDE, 
                                           NBOEUVRE_EXPO_VALIDE, new String[0], 
                                           RESUME_EXPO_VALIDE));
+        
         // Test avec plus de 10 mots-clés
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                          PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE, 
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                          INTITULE_EXPO_VALIDE, 
+                                          PERIODEDEB_EXPO_VALIDE,
+                                          PERIODEFIN_EXPO_VALIDE, 
                                           NBOEUVRE_EXPO_VALIDE, 
-                                          new String[]{"clé1", "clé2", "clé3", "clé4", 
-                                                       "clé5", "clé6", "clé7", "clé8", 
-                                                       "clé9", "clé10", "clé11"}, 
+                                          new String[]{"clé1", "clé2", "clé3", 
+                                                       "clé4", "clé5", "clé6",
+                                                       "clé7", "clé8", "clé9",
+                                                       "clé10", "clé11"}, 
                                           RESUME_EXPO_VALIDE));
-        
+
         // Test avec un mot-clé null ou vide
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                          PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE, 
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                          INTITULE_EXPO_VALIDE, 
+                                          PERIODEDEB_EXPO_VALIDE,
+                                          PERIODEFIN_EXPO_VALIDE, 
                                           NBOEUVRE_EXPO_VALIDE, 
                                           new String[]{"paysage", null}, 
                                           RESUME_EXPO_VALIDE));
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                          PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE, 
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                          INTITULE_EXPO_VALIDE, 
+                                          PERIODEDEB_EXPO_VALIDE,
+                                          PERIODEFIN_EXPO_VALIDE, 
                                           NBOEUVRE_EXPO_VALIDE, 
                                           new String[]{"paysage", ""}, 
                                           RESUME_EXPO_VALIDE));
-        
+
         // Test avec résumé invalide
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                          PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE, 
-                                          NBOEUVRE_EXPO_VALIDE, MOTSCLES_EXPO_VALIDE, null));
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                          INTITULE_EXPO_VALIDE, 
+                                          PERIODEDEB_EXPO_VALIDE,
+                                          PERIODEFIN_EXPO_VALIDE, 
+                                          NBOEUVRE_EXPO_VALIDE,
+                                          MOTSCLES_EXPO_VALIDE, null));
         assertThrows(IllegalArgumentException.class, 
-                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                          PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE, 
-                                          NBOEUVRE_EXPO_VALIDE, MOTSCLES_EXPO_VALIDE, ""));
+                     () -> new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                          INTITULE_EXPO_VALIDE,
+                                          PERIODEDEB_EXPO_VALIDE,
+                                          PERIODEFIN_EXPO_VALIDE, 
+                                          NBOEUVRE_EXPO_VALIDE,
+                                          MOTSCLES_EXPO_VALIDE, ""));
     }
-    
+
     /**
      * Méthode de test pour 
      * {@link application.modele.Exposition#Exposition(java.lang.String, java.lang.String,
@@ -134,9 +189,13 @@ class TestExposition {
      */
     @Test
     void testExpositionValide() {
-        assertDoesNotThrow(() -> new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                                PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE, 
-                                                NBOEUVRE_EXPO_VALIDE, MOTSCLES_EXPO_VALIDE, 
+        
+        assertDoesNotThrow(() -> new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                                INTITULE_EXPO_VALIDE, 
+                                                PERIODEDEB_EXPO_VALIDE,
+                                                PERIODEFIN_EXPO_VALIDE, 
+                                                NBOEUVRE_EXPO_VALIDE,
+                                                MOTSCLES_EXPO_VALIDE, 
                                                 RESUME_EXPO_VALIDE));
     }
 
@@ -145,14 +204,18 @@ class TestExposition {
      */
     @Test
     void testGetIntitule() {
-        Exposition expo = new Exposition(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE, 
-                                         PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE, 
-                                         NBOEUVRE_EXPO_VALIDE, MOTSCLES_EXPO_VALIDE, 
+        
+        Exposition expo = new Exposition(IDENTIFIANT_EXPO_VALIDE,
+                                         INTITULE_EXPO_VALIDE, 
+                                         PERIODEDEB_EXPO_VALIDE,
+                                         PERIODEFIN_EXPO_VALIDE, 
+                                         NBOEUVRE_EXPO_VALIDE,
+                                         MOTSCLES_EXPO_VALIDE, 
                                          RESUME_EXPO_VALIDE);
         assertEquals(INTITULE_EXPO_VALIDE, expo.getIntitule());
     }
 
-   /**
+    /**
      * Méthode de test pour 
      * {@link application.modele.Exposition#getNbOeuvre()}.
      */
@@ -160,7 +223,7 @@ class TestExposition {
     void testGetNbOeuvre() {
         assertEquals(15, EXPO_VALIDES[0].getNbOeuvre());
         assertEquals(45, EXPO_VALIDES[1].getNbOeuvre());
-        
+
         assertNotEquals(20, EXPO_VALIDES[0].getNbOeuvre());
     }
 
@@ -170,10 +233,15 @@ class TestExposition {
      */
     @Test
     void testGetMotsCles() {
-        assertArrayEquals(new String[] {"Romain", "Ayoub", "Esteban", "Baptiste", "paysage", "mars"}, EXPO_VALIDES[0].getMotsCles());
-        assertArrayEquals(new String[] {"cubisme", "oeuvre", "abstrait", "art moderne"}, EXPO_VALIDES[1].getMotsCles());
-        
-        assertNotEquals(new String[] {"abstrait"}, EXPO_VALIDES[1].getMotsCles());
+        assertArrayEquals(new String[] {"Romain", "Ayoub", "Esteban", 
+                                        "Baptiste", "paysage", "mars"}, 
+                          EXPO_VALIDES[0].getMotsCles());
+        assertArrayEquals(new String[] {"cubisme", "oeuvre", "abstrait",
+                                        "art moderne"},
+                          EXPO_VALIDES[1].getMotsCles());
+
+        assertNotEquals(new String[] {"abstrait"}, 
+                        EXPO_VALIDES[1].getMotsCles());
     }
 
     /**
@@ -182,9 +250,12 @@ class TestExposition {
      */
     @Test
     void testGetResume() {
-        assertEquals("Une grande exposition sur des paysages originaux et audacieux", EXPO_VALIDES[0].getResume());
-        assertEquals("Une exposition d'œuvres cubistes influentes.", EXPO_VALIDES[1].getResume());
-        
+        assertEquals("Une grande exposition sur des paysages originaux et"
+                     + " audacieux",
+                     EXPO_VALIDES[0].getResume());
+        assertEquals("Une exposition d'œuvres cubistes influentes.",
+                     EXPO_VALIDES[1].getResume());
+
         assertNotEquals("Exposition moderne", EXPO_VALIDES[1].getResume());
     }
 }
