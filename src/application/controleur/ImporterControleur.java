9afc5203ade0f9010bf4a7237dbbf206c7c6fc86
BaptisteThnrs <baptiste@thenieres.fr>
2024-10-13 02:30:46 +0200

    feat: controleur du menu importer

---
+++ src/application/controleur/ImporterControleur.java
@@ -0,0 +1,49 @@
+/*
+ * ImporterControleur.java                           
+ * 13 oct. 2024
+ * IUT de Rodez, pas de copyright
+ */
+package application.controleur;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * TODO commenter la responsabilité de cette class (SRP)
+ */
+public class ImporterControleur {
+    
+    @FXML
+    private Button btnAide;
+
+    @FXML
+    private Button btnImporterDistant;
+
+    @FXML
+    private Button btnImporterLocal;
+
+    @FXML
+    private Button btnRetour;
+
+    @FXML
+    void btnAideAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnImporterDistantAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnImporterLocalAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRetourAction(ActionEvent event) {
+
+    }
+
+}
