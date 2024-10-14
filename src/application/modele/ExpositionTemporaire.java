91055c72d95451b0b52f6ef46cb9b7a12c4e4150
Esteban Vroemen <esteban.vroemen@iut-rodez.fr>
2024-10-14 11:34:20 +0200

    feat: ExpositionTemporaire corps, docs et tests

--- src/application/modele/ExpositionTemporaire.java
+++ src/application/modele/ExpositionTemporaire.java
@@ -8,16 +8,57 @@ package application.modele;
 import java.time.LocalDate;
 
 /**
- * TODO commenter la responsabilité de cette class (SRP)
+ * Une exposition temporaire sera décrite par :<br>
+ * - un intitulé,<br>
+ * - la période que ses oeuvres recouvrent (deux années),<br>
+ * - le nombre d’œuvres,<br>
+ * - des mots-clés (au plus 10)<br>
+ * - et un résumé constitué de quelques lignes de texte.<br>
+ * - une date de début de l'exposition<br>
+ * - une date de fin de l'exposition<br>
+ * Par exemple, l'exposition "Les paysages impressionnistes" 
+ * regroupera 20 tableaux datés de 1880 à 1895 et sera décrite par
+ * les mots-clés : paysage, impressionnisme, Monet, Pissarro, Sysley
+ * et Signac. Le résumé pourrait être « Très belle exposition mettant
+ * en évidence les approches différentes de grands peintres du
+ * mouvement impressionniste ». Pour une exposition temporaire,
+ * on connaîtra, en plus des éléments précédents, la date de début
+ * et la date de fin de celle-ci. Par exemple, du 17 mars 2025
+ * au 21 juin 2025.
  */
 public class ExpositionTemporaire extends Exposition {
+    
+    private static final String ERREUR_DATE_DEBUT_INVALIDE =
+    """
+    Impossible de créer une exposition temporaire.
+    La référence de la date de début d'une exposition temporaire
+    ne doit pas être nulle.                 
+    """;
+    
+    private static final String ERREUR_DATE_FIN_INVALIDE =
+    """
+    Impossible de créer une exposition temporaire.
+    La référence de la date de fin d'une exposition temporaire ne doit pas être
+    nulle.                
+    """;
+    
+    private static final String ERREUR_DATE_FIN_INFERIEUR =
+    """
+    Impossible de créer une exposition temporaire.
+    La date de fin d'une exposition temporaire ne doit pas être inférieure ou
+    égale à la date de début.                
+    """;
     
     private LocalDate dateDebut;
     
     private LocalDate dateFin;
     
     /**
-     * TODO commenter l'état initial
+     * Initialise une nouvelle exposition temporaire avec un
+     * identifiant, un intitulé, une période avec une année de début
+     * et une année de fin, un nombre d'oeuvres, une liste de mots
+     * clés, un résumé, une date de début et une date de fin.
+     * 
      * @param identifiant l'identifiant/code de l'exposition
      *                    temporaire
      * @param intitule le nom ou la désignation de l'exposition
@@ -30,8 +71,8 @@ public class ExpositionTemporaire extends Exposition {
      * @param motsCles liste de mots clés permettant de retrouver
      *                 l'exposition
      * @param resume le résumé de l'exposition
-     * @param dateDebut
-     * @param dateFin
+     * @param dateDebut la date de début de l'exposition
+     * @param dateFin la date de fin de de l'exposition
      */
     public ExpositionTemporaire(String identifiant, String intitule,
                                 int periodeDebut, int periodeFin, int nbOeuvre,
@@ -40,6 +81,19 @@ public class ExpositionTemporaire extends Exposition {
         
         super(identifiant, intitule, periodeDebut, periodeFin, nbOeuvre,
               motsCles, resume);
+        
+        if (dateDebut == null) {
+            throw new IllegalArgumentException(ERREUR_DATE_DEBUT_INVALIDE);
+        }
+        
+        if (dateFin == null) {
+            throw new IllegalArgumentException(ERREUR_DATE_FIN_INVALIDE);
+        }
+        
+        if (dateFin.compareTo(dateDebut) <= 0) {
+            throw new IllegalArgumentException(ERREUR_DATE_FIN_INFERIEUR);
+        }
+        
         this.dateDebut = dateDebut;
         this.dateFin = dateFin;
     }
---
+++ src/test/modele/TestExpositionTemporaire.java
@@ -0,0 +1,259 @@
+/*
+ * TestExpositionTemporaire.java                           
+ * 14 oct. 2024
+ * IUT de Rodez, pas de copyright
+ */
+package test.modele;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import application.modele.ExpositionTemporaire;
+
+/**
+ * Classe de test pour {@link application.modele.ExpositionTemporaire}
+ * @author Esteban Vroemen
+ * @version 1.0
+ */
+class TestExpositionTemporaire {
+    
+    private final String IDENTIFIANT_EXPO_VALIDE = "E000001";
+    
+    private final String INTITULE_EXPO_VALIDE = "Les paysages impressionnistes";
+    
+    private final int PERIODEDEB_EXPO_VALIDE = 1880;
+    
+    private final int PERIODEFIN_EXPO_VALIDE = 1895;
+    
+    private final int NBOEUVRE_EXPO_VALIDE = 20;
+    
+    private final String[] MOTSCLES_EXPO_VALIDE
+    = {"paysage", "impressionnisme", "Monet", "Pissarro", "Sysley", "Signac"};
+    
+    private final String RESUME_EXPO_VALIDE
+    = "Très belle exposition sur l'impressionnisme.";
+    
+    private final LocalDate DATE_VALIDE = LocalDate.of(2024, 10, 14);
+    
+    private final ExpositionTemporaire[] EXPOSITIONS_TEMP_VALIDES =
+    {
+        new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE,
+                                 PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE,
+                                 NBOEUVRE_EXPO_VALIDE, MOTSCLES_EXPO_VALIDE,
+                                 RESUME_EXPO_VALIDE, LocalDate.of(2005, 5, 28),
+                                 DATE_VALIDE),
+        new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE,
+                                 PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE,
+                                 NBOEUVRE_EXPO_VALIDE, MOTSCLES_EXPO_VALIDE,
+                                 RESUME_EXPO_VALIDE, LocalDate.of(2005, 3, 21),
+                                 DATE_VALIDE),
+        new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE,
+                                 PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE,
+                                 NBOEUVRE_EXPO_VALIDE, MOTSCLES_EXPO_VALIDE,
+                                 RESUME_EXPO_VALIDE, LocalDate.of(2005, 8, 14),
+                                 DATE_VALIDE),
+        new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE, INTITULE_EXPO_VALIDE,
+                                 PERIODEDEB_EXPO_VALIDE, PERIODEFIN_EXPO_VALIDE,
+                                 NBOEUVRE_EXPO_VALIDE, MOTSCLES_EXPO_VALIDE,
+                                 RESUME_EXPO_VALIDE, LocalDate.of(2005, 11, 20),
+                                 DATE_VALIDE),
+    };
+
+    /**
+     * Méthode de test pour
+     * {@link application.modele.ExpositionTemporaire#ExpositionTemporaire(
+     * java.lang.String, java.lang.String, int, int, int, java.lang.String[], java.lang.String, java.time.LocalDate, java.time.LocalDate)}.
+     * Cas uniquement invalides
+     */
+    @Test
+    void testExpositionTemporaireInvalide() {
+        
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE,
+                                                    INTITULE_EXPO_VALIDE,
+                                                    PERIODEDEB_EXPO_VALIDE,
+                                                    PERIODEFIN_EXPO_VALIDE,
+                                                    NBOEUVRE_EXPO_VALIDE,
+                                                    MOTSCLES_EXPO_VALIDE,
+                                                    RESUME_EXPO_VALIDE,
+                                                    null, null));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE,
+                                                    INTITULE_EXPO_VALIDE,
+                                                    PERIODEDEB_EXPO_VALIDE,
+                                                    PERIODEFIN_EXPO_VALIDE,
+                                                    NBOEUVRE_EXPO_VALIDE,
+                                                    MOTSCLES_EXPO_VALIDE,
+                                                    RESUME_EXPO_VALIDE,
+                                                    DATE_VALIDE, null));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE,
+                                                    INTITULE_EXPO_VALIDE,
+                                                    PERIODEDEB_EXPO_VALIDE,
+                                                    PERIODEFIN_EXPO_VALIDE,
+                                                    NBOEUVRE_EXPO_VALIDE,
+                                                    MOTSCLES_EXPO_VALIDE,
+                                                    RESUME_EXPO_VALIDE,
+                                                    null, DATE_VALIDE));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE,
+                                                    INTITULE_EXPO_VALIDE,
+                                                    PERIODEDEB_EXPO_VALIDE,
+                                                    PERIODEFIN_EXPO_VALIDE,
+                                                    NBOEUVRE_EXPO_VALIDE,
+                                                    MOTSCLES_EXPO_VALIDE,
+                                                    RESUME_EXPO_VALIDE,
+                                                    DATE_VALIDE, DATE_VALIDE));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE,
+                                                    INTITULE_EXPO_VALIDE,
+                                                    PERIODEDEB_EXPO_VALIDE,
+                                                    PERIODEFIN_EXPO_VALIDE,
+                                                    NBOEUVRE_EXPO_VALIDE,
+                                                    MOTSCLES_EXPO_VALIDE,
+                                                    RESUME_EXPO_VALIDE,
+                                                    DATE_VALIDE,
+                                                    LocalDate.of(2005, 5, 28)));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE,
+                                                    INTITULE_EXPO_VALIDE,
+                                                    PERIODEDEB_EXPO_VALIDE,
+                                                    PERIODEFIN_EXPO_VALIDE,
+                                                    NBOEUVRE_EXPO_VALIDE,
+                                                    MOTSCLES_EXPO_VALIDE,
+                                                    RESUME_EXPO_VALIDE,
+                                                    DATE_VALIDE,
+                                                    LocalDate.of(2005, 3, 21)));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE,
+                                                    INTITULE_EXPO_VALIDE,
+                                                    PERIODEDEB_EXPO_VALIDE,
+                                                    PERIODEFIN_EXPO_VALIDE,
+                                                    NBOEUVRE_EXPO_VALIDE,
+                                                    MOTSCLES_EXPO_VALIDE,
+                                                    RESUME_EXPO_VALIDE,
+                                                    DATE_VALIDE,
+                                                    LocalDate.of(2005, 8, 14)));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE,
+                                                    INTITULE_EXPO_VALIDE,
+                                                    PERIODEDEB_EXPO_VALIDE,
+                                                    PERIODEFIN_EXPO_VALIDE,
+                                                    NBOEUVRE_EXPO_VALIDE,
+                                                    MOTSCLES_EXPO_VALIDE,
+                                                    RESUME_EXPO_VALIDE,
+                                                    DATE_VALIDE,
+                                                    LocalDate.of(2005, 11, 20)));
+    }
+    
+    /**
+     * Méthode de test pour
+     * {@link application.modele.ExpositionTemporaire#ExpositionTemporaire(
+     * java.lang.String, java.lang.String, int, int, int, java.lang.String[], java.lang.String, java.time.LocalDate, java.time.LocalDate)}.
+     * Cas uniquement valides
+     */
+    @Test
+    void testExpositionTemporaireValide() {
+
+        assertDoesNotThrow(
+                () -> new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE,
+                                               INTITULE_EXPO_VALIDE,
+                                               PERIODEDEB_EXPO_VALIDE,
+                                               PERIODEFIN_EXPO_VALIDE,
+                                               NBOEUVRE_EXPO_VALIDE,
+                                               MOTSCLES_EXPO_VALIDE,
+                                               RESUME_EXPO_VALIDE,
+                                               LocalDate.of(2005, 5, 28),
+                                               DATE_VALIDE));
+        assertDoesNotThrow(
+                () -> new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE,
+                                               INTITULE_EXPO_VALIDE,
+                                               PERIODEDEB_EXPO_VALIDE,
+                                               PERIODEFIN_EXPO_VALIDE,
+                                               NBOEUVRE_EXPO_VALIDE,
+                                               MOTSCLES_EXPO_VALIDE,
+                                               RESUME_EXPO_VALIDE,
+                                               LocalDate.of(2005, 3, 21),
+                                               DATE_VALIDE));
+        assertDoesNotThrow(
+                () -> new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE,
+                                               INTITULE_EXPO_VALIDE,
+                                               PERIODEDEB_EXPO_VALIDE,
+                                               PERIODEFIN_EXPO_VALIDE,
+                                               NBOEUVRE_EXPO_VALIDE,
+                                               MOTSCLES_EXPO_VALIDE,
+                                               RESUME_EXPO_VALIDE,
+                                               LocalDate.of(2005, 8, 14),
+                                               DATE_VALIDE));
+        assertDoesNotThrow(
+                () -> new ExpositionTemporaire(IDENTIFIANT_EXPO_VALIDE,
+                                               INTITULE_EXPO_VALIDE,
+                                               PERIODEDEB_EXPO_VALIDE,
+                                               PERIODEFIN_EXPO_VALIDE,
+                                               NBOEUVRE_EXPO_VALIDE,
+                                               MOTSCLES_EXPO_VALIDE,
+                                               RESUME_EXPO_VALIDE,
+                                               LocalDate.of(2005, 11, 20),
+                                               DATE_VALIDE));
+    }
+
+    /**
+     * Méthode de test pour
+     * {@link application.modele.ExpositionTemporaire#getDateDebut()}.
+     */
+    @Test
+    void testGetDateDebut() {
+        
+        assertEquals(LocalDate.of(2005, 5, 28),
+                     EXPOSITIONS_TEMP_VALIDES[0].getDateDebut());
+        assertEquals(LocalDate.of(2005, 3, 21),
+                     EXPOSITIONS_TEMP_VALIDES[1].getDateDebut());
+        assertEquals(LocalDate.of(2005, 8, 14),
+                     EXPOSITIONS_TEMP_VALIDES[2].getDateDebut());
+        assertEquals(LocalDate.of(2005, 11, 20),
+                     EXPOSITIONS_TEMP_VALIDES[3].getDateDebut());
+        
+        assertNotEquals(null, EXPOSITIONS_TEMP_VALIDES[0].getDateDebut());
+        assertNotEquals(null, EXPOSITIONS_TEMP_VALIDES[1].getDateDebut());
+        assertNotEquals(null, EXPOSITIONS_TEMP_VALIDES[2].getDateDebut());
+        assertNotEquals(null, EXPOSITIONS_TEMP_VALIDES[3].getDateDebut());
+        assertNotEquals(LocalDate.of(2005, 5, 29),
+                        EXPOSITIONS_TEMP_VALIDES[0].getDateDebut());
+        assertNotEquals(LocalDate.of(2005, 3, 22),
+                        EXPOSITIONS_TEMP_VALIDES[1].getDateDebut());
+        assertNotEquals(LocalDate.of(2005, 8, 15),
+                        EXPOSITIONS_TEMP_VALIDES[2].getDateDebut());
+        assertNotEquals(LocalDate.of(2005, 11, 21),
+                        EXPOSITIONS_TEMP_VALIDES[3].getDateDebut());
+    }
+
+    /**
+     * Méthode de test pour
+     * {@link application.modele.ExpositionTemporaire#getDateFin()}.
+     */
+    @Test
+    void testGetDateFin() {
+        
+        assertEquals(DATE_VALIDE, EXPOSITIONS_TEMP_VALIDES[0].getDateFin());
+        assertEquals(DATE_VALIDE, EXPOSITIONS_TEMP_VALIDES[1].getDateFin());
+        assertEquals(DATE_VALIDE, EXPOSITIONS_TEMP_VALIDES[2].getDateFin());
+        assertEquals(DATE_VALIDE, EXPOSITIONS_TEMP_VALIDES[3].getDateFin());
+        
+        assertNotEquals(null, EXPOSITIONS_TEMP_VALIDES[0].getDateFin());
+        assertNotEquals(null, EXPOSITIONS_TEMP_VALIDES[1].getDateFin());
+        assertNotEquals(null, EXPOSITIONS_TEMP_VALIDES[2].getDateFin());
+        assertNotEquals(null, EXPOSITIONS_TEMP_VALIDES[3].getDateFin());
+        assertNotEquals(LocalDate.of(2005, 5, 29),
+                        EXPOSITIONS_TEMP_VALIDES[0].getDateFin());
+        assertNotEquals(LocalDate.of(2005, 3, 22),
+                        EXPOSITIONS_TEMP_VALIDES[1].getDateFin());
+        assertNotEquals(LocalDate.of(2005, 8, 15),
+                        EXPOSITIONS_TEMP_VALIDES[2].getDateFin());
+        assertNotEquals(LocalDate.of(2005, 11, 21),
+                        EXPOSITIONS_TEMP_VALIDES[3].getDateFin());
+    }
+
+}
