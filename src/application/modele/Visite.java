62468fde6777edd65307488b43b3ae45e8a646cd
Esteban Vroemen <esteban.vroemen@iut-rodez.fr>
2024-10-15 14:33:42 +0200

    feat: Visite, docs et quelques tests

--- src/application/modele/Visite.java
+++ src/application/modele/Visite.java
@@ -5,8 +5,11 @@
  */
 package application.modele;
 
+import java.time.LocalDate;
+
 /**
  * Une visite est décrite par :<br>
+ * - son identifiant
  * - l’exposition concernée<br>
  * - le conférencier qui va assurer cette visite<br>
  * - l’horaire de début de la visite<br>
@@ -17,8 +20,200 @@ package application.modele;
  * (par exemple « M.Jacques Dupont » ou « Les amis des musées de
  * Rodez » ou encore « M. Jacques Dupont pour les amis des musées
  * de Rodez »)
+ * @author Esteban Vroemen
  * @version 1.0
  */
 public class Visite {
+    
+    private static final String ERREUR_IDENTIFIANT_INVALIDE =
+    """
+    Impossible de créer une visite.
+    La référence de l'identifiant de la visite ne doit pas être nulle ou
+    l'identifiant ne doit pas être vide.                
+    """;
+    
+    private static final String ERREUR_HORAIRE_DEBUT_INVALIDE =
+    """
+    Impossible de créer une visite.
+    L'horaire de début de la visite ne doit pas être négatif ou supérieur à
+    1439.
+    Exemples : 0 = 0h00, 1439 = 23h59, 612 = 10h12
+    """;
+    
+    private static final String ERREUR_DATE_INVALIDE =
+    """
+    Impossible de créer une visite.
+    La référence de la date de la visite ne doit pas être nulle.                
+    """;
+    
+    private static final String ERREUR_CLIENT_INVALIDE =
+    """
+    Impossible de créer une visite.
+    La référence du client de la visite ne doit pas être nulle.                   
+    """;
+    
+    private static final String ERREUR_EXPOSITION_INVALIDE =
+    """
+    Impossible de créer une visite.
+    La référence de l'exposition de la visite ne doit pas être nulle.                
+    """;
+    
+    private static final String ERREUR_EXPLOYE_INVALIDE =
+    """
+    Impossible de créer une visite.
+    La référence de l'employé de la visite ne doit pas être nulle.                   
+    """;
+    
+    private static final String ERREUR_CONFEFENCIER_INVALIDE =
+    """
+    Impossible de créer une visite.
+    La référence du conférencier de la visite ne doit pas être nulle.                   
+    """;
+    
+    private String identifiant;
+    
+    private int horaireDebut;
+    
+    private LocalDate date;
+    
+    private Client client;
+    
+    private Exposition exposition;
+    
+    private Employe employe;
+    
+    private Conferencier conferencier;
+
+    /**
+     * Initialise une visite avec son identifiant, son horaire de
+     * début, sa date, le client, l'exposition, l'employé, et le
+     * conférencier associés à cette visite.
+     * 
+     * @param identifiant l'identifiant de la visite
+     * @param horaireDebut l'horaire de la visite
+     *                     (en nombre de minutes)
+     * @param date la date de la visite
+     * @param client le client de la visite
+     * @param exposition l'exposition de la visite
+     * @param employe l'employé de la visite
+     * @param conferencier le conférencier de la visite
+     * @throws IllegalArgumentException si la référence de
+     *                                  l'identifiant est nulle ou
+     *                                  l'identifiant est vide
+     * @throws IllegalArgumentException si la valeur de l'horaire de
+     *                                  début est négative ou
+     *                                  supérieur à 1439
+     * @throws IllegalArgumentException si la référence de la date
+     *                                  est nulle
+     * @throws IllegalArgumentException si la référence du client est
+     *                                  nulle
+     * @throws IllegalArgumentException si la référence de
+     *                                  l'exposition est nulle
+     * @throws IllegalArgumentException si la référence de l'employe
+     *                                  est nulle
+     * @throws IllegalArgumentException si la référence du
+     *                                  conférencier est nulle
+     */
+    public Visite(String identifiant, int horaireDebut, LocalDate date,
+                  Client client, Exposition exposition, Employe employe,
+                  Conferencier conferencier) {
+        
+        if (identifiant == null || identifiant.isBlank()) {
+            throw new IllegalArgumentException(ERREUR_IDENTIFIANT_INVALIDE);
+        }
+        
+        /* 
+         * 24 * 60 = 1440
+         * Il y a 1440 minutes dans une journée
+         * 0 = 0h00, 1439 = 23h59
+         */
+        if (horaireDebut < 0 || horaireDebut >= 1440) {
+            throw new IllegalArgumentException(ERREUR_HORAIRE_DEBUT_INVALIDE);
+        }
+        
+        if (date == null) {
+            throw new IllegalArgumentException(ERREUR_DATE_INVALIDE);
+        }
+        
+        if (client == null) {
+            throw new IllegalArgumentException(ERREUR_CLIENT_INVALIDE);
+        }
+        
+        if (exposition == null) {
+            throw new IllegalArgumentException(ERREUR_EXPOSITION_INVALIDE);
+        }
+        
+        if (employe == null) {
+            throw new IllegalArgumentException(ERREUR_EXPLOYE_INVALIDE);
+        }
+        
+        if (conferencier == null) {
+            throw new IllegalArgumentException(ERREUR_CONFEFENCIER_INVALIDE);
+        }
+        
+        this.identifiant = identifiant.trim();
+        this.horaireDebut = horaireDebut;
+        this.date = date;
+        this.client = client;
+        this.exposition = exposition;
+        this.employe = employe;
+        this.conferencier = conferencier;
+    }
+
+    /**
+     * Récupère l'identifiant de la visite.
+     * @return identifiant l'identifiant de la visite
+     */
+    public String getIdentifiant() {
+        return identifiant;
+    }
+
+    /**
+     * Récupère l'horaire de début de la visite.
+     * @return horaireDebut l'horaire de début de la visite
+     */
+    public int getHoraireDebut() {
+        return horaireDebut;
+    }
+
+    /**
+     * Récupère la date de la visite.
+     * @return date
+     */
+    public LocalDate getDate() {
+        return date;
+    }
+
+    /**
+     * Récupère le client de la visite.
+     * @return client
+     */
+    public Client getClient() {
+        return client;
+    }
+
+    /**
+     * Récupère l'exposition de la visite.
+     * @return exposition
+     */
+    public Exposition getExposition() {
+        return exposition;
+    }
+
+    /**
+     * Récupère l'employé de la visite.
+     * @return employe
+     */
+    public Employe getEmploye() {
+        return employe;
+    }
+
+    /**
+     * Récupère le conférencier de la visite.
+     * @return conferencier
+     */
+    public Conferencier getConferencier() {
+        return conferencier;
+    }
 
 }
---
+++ src/test/modele/TestVisite.java
@@ -0,0 +1,336 @@
+/*
+ * TestVisite.java                           
+ * 14 oct. 2024
+ * IUT de Rodez, pas de copyright
+ */
+package test.modele;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import application.modele.Client;
+import application.modele.Conferencier;
+import application.modele.Employe;
+import application.modele.Exposition;
+import application.modele.Visite;
+
+/**
+ * Classe de test pour {@link application.modele.Visite}
+ * @author Esteban Vroemen
+ * @version 1.0
+ */
+class TestVisite {
+    
+    private final String IDENTIFIANT_VISITE_VALIDE = "V000001";
+    
+    private final int HORAIRE_DEBUT_VISITE_VALIDE = 610; // 10h10
+    
+    private final LocalDate DATE_VISITE_VALIDE = LocalDate.of(2024, 10, 12);
+    
+    private final Client CLIENT_VISITE_VALIDE 
+    = new Client("Esteban", "123456789");
+    
+    private final Exposition EXPOSITION_VISITE_VALIDE
+    = new Exposition("E000001", "Les paysages impressionnistes", 1780, 1810, 25,
+                     new String[] {"paysage", "impressionnisme",
+                                   "Monet", "Pissarro", "Sysley",
+                                   "Signac"},
+                     "Très belle exposition sur l'impressionnisme.");
+    
+    private final Employe EMPLOYE_VISITE_VALIDE
+    = new Employe("N000001", "Mathieu", "Bernoulli", "1234");
+    
+    // TODO Compléter avec le constructeur de conferencier
+    private final Conferencier CONFERENCIER_VISITE_VALIDE
+    = new Conferencier();
+    
+    private final Visite[] VISITES_VALIDES =
+    {
+        new Visite(IDENTIFIANT_VISITE_VALIDE, HORAIRE_DEBUT_VISITE_VALIDE,
+                   DATE_VISITE_VALIDE, CLIENT_VISITE_VALIDE,
+                   EXPOSITION_VISITE_VALIDE, EMPLOYE_VISITE_VALIDE,
+                   CONFERENCIER_VISITE_VALIDE),
+        new Visite("V000002", 1500, LocalDate.of(2024, 11, 20),
+                   new Client("Alice", "987654321"),
+                   new Exposition("E000002", "Le surréalisme en couleurs", 1920,
+                                  1950, 30,
+                                  new String[] {"surréalisme", "Dali", 
+                                                "Magritte", "Ernst", "Tanguy", 
+                                                "Man Ray"},
+                                  "Une exploration fascinante des couleurs dans"
+                                  + " le mouvement surréaliste."),
+                   new Employe("N000002", "Chloé", "Fourier", "5678"),
+                   new Conferencier()),
+        new Visite("V000003", 1800, LocalDate.of(2024, 12, 5),
+                   new Client("Benjamin", "456123789"),
+                   new Exposition("E000003", "L'Art abstrait moderne", 1945,
+                                  1980, 40,
+                                  new String[] {"abstraction", "Kandinsky", 
+                                                "Pollock", "Rothko", 
+                                                "Mondrian"},
+                                  "Un voyage au cœur de l'art abstrait."),
+                   new Employe("N000003", "Julien", "Descartes", "7890"),
+                   new Conferencier()),
+        new Visite("V000004", 2000, LocalDate.of(2025, 1, 10),
+                   new Client("Camille", "321654987"),
+                   new Exposition("E000004", "Le cubisme décomposé", 1907, 1925,
+                                  35, new String[] {"cubisme", "Picasso", 
+                                                    "Braque", "Gris"},
+                                  "Un décryptage fascinant du cubisme."),
+                   new Employe("N000004", "Luc", "Galois", "9012"),
+                   new Conferencier()),
+        new Visite("V000005", 1700, LocalDate.of(2025, 2, 28),
+                   new Client("Diane", "789123456"),
+                   new Exposition("E000005", "Les sculptures grecques", -500,
+                                  -100, 20,
+                                  new String[] {"sculpture", "Grèce antique", 
+                                                "Phidias", "Praxitèle"},
+                                  "Un retour sur l'âge d'or de la sculpture "
+                                  + "grecque."),
+                   new Employe("N000005", "Clara", "Euler", "3456"),
+                   new Conferencier())
+    };
+
+    /**
+     * Méthode de test pour
+     * {@link application.modele.Visite#Visite(
+     * java.lang.String, int, java.time.LocalDate, application.modele.Client, application.modele.Exposition, application.modele.Employe, application.modele.Conferencier)}.
+     * Cas uniquement invalides
+     */
+    @Test
+    void testVisiteInvalide() {
+        
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Visite(null,
+                                      HORAIRE_DEBUT_VISITE_VALIDE,
+                                      DATE_VISITE_VALIDE, 
+                                      CLIENT_VISITE_VALIDE,
+                                      EXPOSITION_VISITE_VALIDE, 
+                                      EMPLOYE_VISITE_VALIDE,
+                                      CONFERENCIER_VISITE_VALIDE));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Visite("",
+                                      HORAIRE_DEBUT_VISITE_VALIDE,
+                                      DATE_VISITE_VALIDE, 
+                                      CLIENT_VISITE_VALIDE,
+                                      EXPOSITION_VISITE_VALIDE, 
+                                      EMPLOYE_VISITE_VALIDE,
+                                      CONFERENCIER_VISITE_VALIDE));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Visite(IDENTIFIANT_VISITE_VALIDE,
+                                      -1,
+                                      DATE_VISITE_VALIDE, 
+                                      CLIENT_VISITE_VALIDE,
+                                      EXPOSITION_VISITE_VALIDE, 
+                                      EMPLOYE_VISITE_VALIDE,
+                                      CONFERENCIER_VISITE_VALIDE));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Visite(IDENTIFIANT_VISITE_VALIDE,
+                                      1440,
+                                      DATE_VISITE_VALIDE, 
+                                      CLIENT_VISITE_VALIDE,
+                                      EXPOSITION_VISITE_VALIDE, 
+                                      EMPLOYE_VISITE_VALIDE,
+                                      CONFERENCIER_VISITE_VALIDE));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Visite(IDENTIFIANT_VISITE_VALIDE,
+                                      -999,
+                                      DATE_VISITE_VALIDE, 
+                                      CLIENT_VISITE_VALIDE,
+                                      EXPOSITION_VISITE_VALIDE, 
+                                      EMPLOYE_VISITE_VALIDE,
+                                      CONFERENCIER_VISITE_VALIDE));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Visite(IDENTIFIANT_VISITE_VALIDE,
+                                      44444,
+                                      DATE_VISITE_VALIDE, 
+                                      CLIENT_VISITE_VALIDE,
+                                      EXPOSITION_VISITE_VALIDE, 
+                                      EMPLOYE_VISITE_VALIDE,
+                                      CONFERENCIER_VISITE_VALIDE));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Visite(IDENTIFIANT_VISITE_VALIDE,
+                                      HORAIRE_DEBUT_VISITE_VALIDE,
+                                      null, 
+                                      CLIENT_VISITE_VALIDE,
+                                      EXPOSITION_VISITE_VALIDE, 
+                                      EMPLOYE_VISITE_VALIDE,
+                                      CONFERENCIER_VISITE_VALIDE));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Visite(IDENTIFIANT_VISITE_VALIDE,
+                                      HORAIRE_DEBUT_VISITE_VALIDE,
+                                      DATE_VISITE_VALIDE, 
+                                      null,
+                                      EXPOSITION_VISITE_VALIDE, 
+                                      EMPLOYE_VISITE_VALIDE,
+                                      CONFERENCIER_VISITE_VALIDE));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Visite(IDENTIFIANT_VISITE_VALIDE,
+                                      HORAIRE_DEBUT_VISITE_VALIDE,
+                                      DATE_VISITE_VALIDE, 
+                                      CLIENT_VISITE_VALIDE,
+                                      null, 
+                                      EMPLOYE_VISITE_VALIDE,
+                                      CONFERENCIER_VISITE_VALIDE));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Visite(IDENTIFIANT_VISITE_VALIDE,
+                                      HORAIRE_DEBUT_VISITE_VALIDE,
+                                      DATE_VISITE_VALIDE, 
+                                      CLIENT_VISITE_VALIDE,
+                                      EXPOSITION_VISITE_VALIDE, 
+                                      null,
+                                      CONFERENCIER_VISITE_VALIDE));
+        assertThrows(IllegalArgumentException.class, 
+                     () -> new Visite(IDENTIFIANT_VISITE_VALIDE,
+                                      HORAIRE_DEBUT_VISITE_VALIDE,
+                                      DATE_VISITE_VALIDE, 
+                                      CLIENT_VISITE_VALIDE,
+                                      EXPOSITION_VISITE_VALIDE, 
+                                      EMPLOYE_VISITE_VALIDE,
+                                      null));
+    }
+    
+    /**
+     * Méthode de test pour
+     * {@link application.modele.Visite#Visite(
+     * java.lang.String, int, java.time.LocalDate, application.modele.Client, application.modele.Exposition, application.modele.Employe, application.modele.Conferencier)}.
+     * Cas uniquement valides
+     */
+    @Test
+    void testVisiteValide() {
+
+        assertDoesNotThrow(() -> new Visite(IDENTIFIANT_VISITE_VALIDE,
+                                            HORAIRE_DEBUT_VISITE_VALIDE,
+                                            DATE_VISITE_VALIDE, 
+                                            CLIENT_VISITE_VALIDE,
+                                            EXPOSITION_VISITE_VALIDE, 
+                                            EMPLOYE_VISITE_VALIDE,
+                                            CONFERENCIER_VISITE_VALIDE));
+        assertDoesNotThrow(
+                () -> new Visite("V000002", 1500, LocalDate.of(2024, 11, 20),
+                                 new Client("Alice", "987654321"),
+                                 new Exposition("E000002",
+                                                "Le surréalisme en couleurs",
+                                                1920, 1950, 30,
+                                                new String[] {"surréalisme", 
+                                                              "Dali", 
+                                                              "Magritte", 
+                                                              "Ernst", 
+                                                              "Tanguy", 
+                                                              "Man Ray"},
+                                                "Une exploration fascinante des"
+                                                + " couleurs dans le mouvement"
+                                                + " surréaliste."),
+                                 new Employe("N000002", "Chloé", "Fourier", 
+                                             "5678"),
+                                 new Conferencier()));
+        assertDoesNotThrow(
+                () -> new Visite("V000003", 1800, LocalDate.of(2024, 12, 5),
+                                 new Client("Benjamin", "456123789"),
+                                 new Exposition("E000003",
+                                                "L'Art abstrait moderne",
+                                                1945, 1980, 40,
+                                                new String[] {"abstraction", 
+                                                              "Kandinsky", 
+                                                              "Pollock", 
+                                                              "Rothko", 
+                                                              "Mondrian"},
+                                                "Un voyage au cœur de l'art "
+                                                + "abstrait."),
+                                 new Employe("N000003", "Julien", "Descartes", 
+                                             "7890"),
+                                 new Conferencier()));
+                                 
+            assertDoesNotThrow(
+                () -> new Visite("V000004", 2000, LocalDate.of(2025, 1, 10),
+                                 new Client("Camille", "321654987"),
+                                 new Exposition("E000004",
+                                                "Le cubisme décomposé",
+                                                1907, 1925, 35,
+                                                new String[] {"cubisme", 
+                                                              "Picasso", 
+                                                              "Braque", 
+                                                              "Gris"},
+                                                "Un décryptage fascinant du "
+                                                + "cubisme."),
+                                 new Employe("N000004", "Luc", "Galois", 
+                                             "9012"),
+                                 new Conferencier()));
+                                 
+            assertDoesNotThrow(
+                () -> new Visite("V000005", 1700, LocalDate.of(2025, 2, 28),
+                                 new Client("Diane", "789123456"),
+                                 new Exposition("E000005",
+                                                "Les sculptures grecques",
+                                                -500, -100, 20,
+                                                new String[] {"sculpture", 
+                                                              "Grèce antique", 
+                                                              "Phidias", 
+                                                              "Praxitèle"},
+                                                "Un retour sur l'âge d'or de la"
+                                                + " sculpture grecque."),
+                                 new Employe("N000005", "Clara", "Euler", 
+                                             "3456"),
+                                 new Conferencier()));
+    }
+
+    /**
+     * Méthode de test pour {@link application.modele.Visite#getIdentifiant()}.
+     */
+    @Test
+    void testGetIdentifiant() {
+        
+    }
+
+    /**
+     * Méthode de test pour {@link application.modele.Visite#getHoraireDebut()}.
+     */
+    @Test
+    void testGetHoraireDebut() {
+        
+    }
+
+    /**
+     * Méthode de test pour {@link application.modele.Visite#getDate()}.
+     */
+    @Test
+    void testGetDate() {
+        
+    }
+
+    /**
+     * Méthode de test pour {@link application.modele.Visite#getClient()}.
+     */
+    @Test
+    void testGetClient() {
+        
+    }
+
+    /**
+     * Méthode de test pour {@link application.modele.Visite#getExposition()}.
+     */
+    @Test
+    void testGetExposition() {
+        
+    }
+
+    /**
+     * Méthode de test pour {@link application.modele.Visite#getEmploye()}.
+     */
+    @Test
+    void testGetEmploye() {
+        
+    }
+
+    /**
+     * Méthode de test pour {@link application.modele.Visite#getConferencier()}.
+     */
+    @Test
+    void testGetConferencier() {
+        
+    }
+
+}
