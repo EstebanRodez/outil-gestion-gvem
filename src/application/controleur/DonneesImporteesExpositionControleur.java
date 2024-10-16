bb53f33f1949512c4c1365deea7921dfbbdbe6f0
BAPTISTE THENIERES <baptiste@thenieres.fr>
2024-10-16 08:45:27 +0200

    feat: redirection de la liste des donnees importees sur les expositions vers l'accueil et fonctionnement du bouton 'retour'

--- src/application/controleur/DonneesImporteesExpositionControleur.java
+++ src/application/controleur/DonneesImporteesExpositionControleur.java
@@ -5,8 +5,13 @@
  */
 package application.controleur;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -29,23 +34,31 @@ public class DonneesImporteesExpositionControleur {
     private Button btnRetour;
 
     @FXML
-    void retourAccueil(ActionEvent event) {
+    void retourAccueilAction(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/vue/accueilVue.fxml"));
+        Parent accueilVue = loader.load();
+        AccueilControleur controleur = loader.getController();
+        controleur.setFenetreAppli(fenetreAppli);
+        fenetreAppli.setScene(new Scene(accueilVue));
+    }
+
+    @FXML
+    void quitterAction(ActionEvent event) {
+        fenetreAppli.hide();
+    }
+
+    @FXML
+    void aideAction(ActionEvent event) {
 
     }
 
     @FXML
-    void quitterPartie(ActionEvent event) {
-
-    }
-
-    @FXML
-    void afficherManuelUtilisateur(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnRetourAction(ActionEvent event) {
-
+    void btnRetourAction(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/vue/menuDonneesImporterVue.fxml"));
+        Parent menuDonneesImporterVue = loader.load();
+        MenuDonneesImporterControleur controleur = loader.getController();
+        controleur.setFenetreAppli(fenetreAppli);
+        fenetreAppli.setScene(new Scene(menuDonneesImporterVue));
     }
 
 }
