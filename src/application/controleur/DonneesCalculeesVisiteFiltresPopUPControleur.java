1962cb5e81a8ddeeff6b7235d3101edb8cf86b8f
BAPTISTE THENIERES <baptiste@thenieres.fr>
2024-10-25 14:34:47 +0200

    feat: vue et controleur d'une popup filtre sur les visites

---
+++ src/application/controleur/DonneesCalculeesVisiteFiltresPopUPControleur.java
@@ -0,0 +1,24 @@
+/*
+ * DonneesCalculeesVisiteFiltresPopUPControleur.java                           
+ * 25 oct. 2024
+ * IUT de Rodez, pas de copyright
+ */
+package application.controleur;
+
+import javafx.stage.Stage;
+
+/**
+ * TODO commenter la responsabilité de cette class (SRP)
+ */
+public class DonneesCalculeesVisiteFiltresPopUPControleur {
+    
+    private Stage fenetreAppli;
+    
+    /**
+     * Définit la fenêtre de l'application.
+     * @param fenetreAppli
+     */
+    public void setFenetreAppli(Stage fenetreAppli) {
+      this.fenetreAppli = fenetreAppli;
+    }
+}
